0ef0e0b1d3a82efcd4d1a4d6dfb595a15eaeba9e
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2021-11-11 18:53:56 -0300

    Removed mini menu, using KISS toogle menu

--- containers/frontend/src/App.vue
+++ containers/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="app">
     <v-app-bar app clipped-left dark>
-      <v-app-bar-nav-icon @click="setToogleMenu(!toogleMenu)">
+      <v-app-bar-nav-icon @click="toogleMenu = !toogleMenu">
       </v-app-bar-nav-icon>
 
       <v-toolbar-title>{{ this.$route.name[1] }}</v-toolbar-title>
@@ -24,33 +24,12 @@
         <router-view></router-view>
       </v-container>
     </v-main>
-
-    <v-navigation-drawer
-      app
-      mini-variant
-      mini-variant-width="56"
-      clipped
-      stateless
-      dark
-      style="z-index: 5"
-      v-model="toogleMini"
-    >
-      <v-list dense color="blue-gray--text" class="pt-0">
-        <v-list-item to="/">
-          <v-list-item-icon>
-            <v-icon>dashboard_customize</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Dashboards</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    
     <v-navigation-drawer
       app
       clipped
       dark
-      v-model="toogleMenu"
-      :class="toogleMini ? 'ml-lg-14' : ''"
+      v-model="toogleMenu"      
     >
       <pu-navigation />
     </v-navigation-drawer>
@@ -65,37 +44,24 @@ export default {
   components: {
     puNavigation,
   },
-  data: () => ({}),
-  computed: {
-    toogleMenu() {
-      return this.$store.state.navigation.toogleMenu;
-    },
-    toogleMini() {
-      return this.$store.state.navigation.toogleMini;
-    },
-  },
-  methods: {
-    setToogleMenu(value) {
-      this.$store.commit("navigation/setToogleMenu", { value: value });
-    },
-    setToogleMini(value) {
-      this.$store.commit("navigation/setToogleMini", { value: value });
-    },
-  },
+  data: () => ({
+    toogleMenu: false
+  })
 };
 </script>
 
 <style>
 
 .pu-main-container {
-  max-width: 100% !important;
+  width: 100%;
+  max-width: 100%;
 }
 
 .pu-view {
   background-color: #121212;
   height: 100%;
   width: 100%;
-  max-width: 100% !important;
+  max-width: 100%;
 }
 
 </style>
