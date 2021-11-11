315e426d6682d3955c184791904b975da4cccb7d
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2021-11-11 18:42:08 -0300

    Removed unused components and views

--- containers/frontend/src/router/index.js
+++ containers/frontend/src/router/index.js
@@ -11,15 +11,6 @@ const routes = [
     component: () => import('../views/Dashboards.vue')
   },
   {
-    path: '/about',
-    name: ['About', 'About'],
-    title: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/queries/stats',
     name: ['Stats', 'Stats'],
     title: 'Stats',
