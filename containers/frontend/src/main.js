e4a429089b3edf7f7bbc7374342c14c43e9ecf89
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2021-11-15 19:10:26 -0300

    Carbon charts addedd

--- containers/frontend/src/main.js
+++ containers/frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import '@carbon/charts/styles.css'
+import chartsVue from "@carbon/charts-vue"
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
@@ -12,6 +14,7 @@ new Vue({
   router,
   store,
   vuetify,
+  chartsVue,
   render: h => h(App)
 }).$mount('#app')
 
--- containers/frontend/src/views/queries/Stats.vue
+++ containers/frontend/src/views/queries/Stats.vue
@@ -1,10 +1,54 @@
 <template>
   <div>
     <h1>Querie Stats</h1>
+    <div>
+    <ccv-stacked-bar-chart :data="stackedBarData" :options="stackedBarOptions"></ccv-stacked-bar-chart>
+    </div>
   </div>
 </template>
 
 <script>
-export default {  
-};
+import '@carbon/charts/styles.css'
+import { CcvStackedBarChart } from "@carbon/charts-vue"
+
+export default {
+  components: {
+    CcvStackedBarChart
+  },
+  data() {
+    return {
+      stackedBarData: {
+        labels: ["Quantity", "Leads", "Sold", "Restocking", "Misc"],
+        datasets: [
+          {
+            label: "Dataset 1",
+            data: [65000, 29123, 35213, 51213, 16932]
+          },
+          {
+            label: "Dataset 2",
+            data: [32432, 21312, 56456, 21312, 34234]
+          },
+          {
+            label: "Dataset 3",
+            data: [12312, 23232, 34232, 12312, 34234]
+          }
+        ]
+      }
+      ,
+      stackedBarOptions: {
+        axes: {
+          left: {
+            primary: true,
+            stacked: true
+          },
+          bottom: {
+            scaleType: "labels",
+            secondary: true
+          }
+        },
+        height: "800px",        
+      }
+    }
+  }
+}
 </script>
