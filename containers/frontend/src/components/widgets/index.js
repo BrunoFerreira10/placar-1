4711b4766f4a8529e61f91c92c4e751f4eb9d784
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2021-11-16 22:10:33 -0300

    Created Mock Carbon charts

---
+++ containers/frontend/src/components/widgets/index.js
@@ -0,0 +1,9 @@
+import CarbonLineChartMock from "@/components/widgets/CarbonLineChartMock.vue"
+import CarbonGaugeChartMock from "@/components/widgets/CarbonGaugeChartMock.vue"
+import CarbonMeterChartMock from "@/components/widgets/CarbonMeterChartMock.vue"
+
+export default {
+  CarbonLineChartMock,
+  CarbonGaugeChartMock,
+  CarbonMeterChartMock
+}
