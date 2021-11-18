e1cb8e11c62606cabeb2927551cc1157582b275c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2021-11-18 17:58:27 -0300

    Adding and removing itens on Dashboard

--- containers/frontend/src/views/Dashboards.vue
+++ containers/frontend/src/views/Dashboards.vue
@@ -1,7 +1,7 @@
 <template >
-  <v-container class="pu-view ma-0 pa-0">
+  <v-container class="pu-view pu-view-dashboard pa-0">
     <grid-layout
-      id="pu-dashboard-grid"
+      class="pu-dashboard-grid"
       :layout.sync="layout"
       :col-num="12"
       :row-height="10"
@@ -20,9 +20,23 @@
         :h="item.h"
         :i="item.i"
       >
+        <span class="pu-widget-btn-close" @click="removeItem(item.i)">x</span>
         <component :is="item.widget" :props="item.props"></component>
       </grid-item>
     </grid-layout>
+
+    <v-btn
+      color="indigo"
+      class="pu-btn-add-widget"
+      fab
+      small
+      absolute
+      bottom
+      right
+      @click="addItem('KickstartLineChartMock')"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
   </v-container>
 </template>
 
@@ -34,40 +48,102 @@ export default {
   components: {
     GridLayout,
     GridItem,
-    ...Widgets
+    ...Widgets,
   },
   data: () => ({
+    index: 0,
     layout: [
       {
-        x: 0, y: 0, w: 4,h: 12,i: "0",
-        widget: "CarbonLineChartMock", props: { },        
+        x: 0,
+        y: 0,
+        w: 4,
+        h: 12,
+        i: "0",
+        widget: "CarbonLineChartMock",
+        props: {},
       },
       {
-        x: 7, y: 0, w: 2, h: 8, i: "1", 
-        widget: "CarbonGaugeChartMock", props: { },        
+        x: 7,
+        y: 0,
+        w: 2,
+        h: 8,
+        i: "1",
+        widget: "CarbonGaugeChartMock",
+        props: {},
       },
       {
-        x: 4, y: 0, w: 3, h: 4, i: "2",
-        widget: "CarbonMeterChartMock", props: { valor: 75 },
-      },      
-      {
-        x: 4, y: 4, w: 3, h: 4, i: "3",
-        widget: "CarbonMeterChartMock", props: { valor: 85 },
-      },      
-      {
-        x: 4, y: 8, w: 3, h: 4, i: "4",
-        widget: "CarbonMeterChartMock", props: { valor: 95 },
+        x: 4,
+        y: 0,
+        w: 3,
+        h: 4,
+        i: "2",
+        widget: "CarbonMeterChartMock",
+        props: { valor: 75 },
       },
       {
-        x: 0, y: 12, w: 4,h: 12,i: "5",
-        widget: "KickstartLineChartMock", props: { },        
+        x: 4,
+        y: 4,
+        w: 3,
+        h: 4,
+        i: "3",
+        widget: "CarbonMeterChartMock",
+        props: { valor: 85 },
       },
       {
-        x: 0, y: 12, w: 4,h: 12,i: "6",
-        widget: "KickstartAreaChartMock", props: { },        
+        x: 4,
+        y: 8,
+        w: 3,
+        h: 4,
+        i: "4",
+        widget: "CarbonMeterChartMock",
+        props: { valor: 95 },
+      },
+      {
+        x: 0,
+        y: 12,
+        w: 4,
+        h: 12,
+        i: "5",
+        widget: "KickstartLineChartMock",
+        props: {},
+      },
+      {
+        x: 0,
+        y: 12,
+        w: 4,
+        h: 12,
+        i: "6",
+        widget: "KickstartAreaChartMock",
+        props: {},
       },
     ],
-  }),  
+  }),
+  mounted(){
+    this.index = this.layout.length;
+  },
+  methods: {
+    removeItem(itemId) {
+      const index = this.layout.map((item) => item.i).indexOf(itemId);
+      this.layout.splice(index, 1);
+    },
+    addItem: function (widget) {
+    
+    const lastRow = this.layout.reduce((lastRow, item) => item.y + item.h > lastRow ? item.y + item.h : lastRow, 0)
+
+    this.layout.push({
+      x: 0,
+      y: lastRow,
+      w: 4,
+      h: 12,
+      i: this.index,
+      widget: widget,
+      props: {},
+    });
+    
+    this.index++;
+  },
+  },
+  
 };
 </script>
 
@@ -80,15 +156,29 @@ export default {
   background-color: rgba(255, 255, 255, 0.08);
   height: 100%;
   width: 100%;
+  padding-top: 15px;
   overflow: hidden;
 }
 
 .pu-widget h3 {
   color: #fff;
 }
+
+.pu-widget-btn-close {
+  position: absolute;
+  top: -2px;
+  right: 5px;
+  cursor: pointer;
+}
 </style>
 
 <style scoped>
+.pu-view-dashboard {
+  margin-bottom: 200px;
+}
 
-
+.pu-btn-add-widget {
+  bottom: 15px !important;
+  position: fixed;
+}
 </style>
